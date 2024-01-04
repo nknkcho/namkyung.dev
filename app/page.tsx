d47fc01bbff433689f0a9e45bbeff2eeedd52af8
nknkcho <srirachacho@gmail.com>
2024-01-04 23:47:47 +0900

    Update: 홈페이지 초기 레이아웃 업데이트 (제목, 네비게이션)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+
 const Home = () => {
-  return <div>hi</div>;
+  return (
+    <div>
+      <h1>
+        <Link href="">@nkakmk</Link>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/about">about</Link>
+          </li>
+          <li>
+            <Link href="/tags">tags</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Home;
