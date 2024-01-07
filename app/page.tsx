5575b28bbf2004c93c3e2448089dbdb580fdfc11
nknkcho <srirachacho@gmail.com>
2024-01-08 01:02:01 +0900

    Update: /public 경로에 있는 마크다운 파일 받아오는 함수 추가 및 홈페이지에 게시물 링크 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,48 @@
+import { readdir, readFile } from 'fs/promises';
+import matter from 'gray-matter';
 import Link from 'next/link';
 
-const Home = () => {
+type PostData = {
+  slug: string;
+  [p: string]: string;
+};
+
+const getPosts = async (): Promise<PostData[]> => {
+  const entries = await readdir('./public/', { withFileTypes: true });
+  const dirs = entries.filter(entry => entry.isDirectory()).map(entry => entry.name);
+  const fileContents = await Promise.all(
+    dirs.map(dir => readFile(`./public/${dir}/index.md`, 'utf-8'))
+  );
+  const posts: PostData[] = dirs.map((slug, i) => {
+    const fileContent = fileContents[i];
+    const { data } = matter(fileContent);
+
+    return {
+      slug,
+      ...data,
+    };
+  });
+
+  posts.sort((a, b) => {
+    return Date.parse(a.date) < Date.parse(b.date) ? 1 : -1;
+  });
+
+  return posts;
+};
+const Home = async () => {
+  const posts = await getPosts();
+
   return (
     <div>
-      <h1>
-        <Link href="">@nkakmk</Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/about">about</Link>
-          </li>
-          <li>
-            <Link href="/tags">tags</Link>
-          </li>
-        </ul>
-      </nav>
+      {posts.map(post => (
+        <Link key={post.slug} href={`/${post.slug}`} className={'block py-3'}>
+          <article>
+            <h2>{post.title}</h2>
+            <p>{post.date}</p>
+            <p>{post.spoiler}</p>
+          </article>
+        </Link>
+      ))}
     </div>
   );
 };
