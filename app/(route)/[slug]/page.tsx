2a2e8e3de74fb7c31c8c4af856af4affc634f047
nknkcho <srirachacho@gmail.com>
2024-01-08 23:22:06 +0900

    Update: 정적 생성을 위한 generateStaticParams 적용, mdx-bundler 적용

--- app/(route)/[slug]/page.tsx
+++ app/(route)/[slug]/page.tsx
@@ -1,9 +1,41 @@
-const PostPage = ({ params }) => {
+import React from 'react';
+import { readdir, readFile } from 'fs/promises';
+import Post from '../../_components/Post';
+import { bundleMDX } from 'mdx-bundler';
+
+const PostPage = async ({ params }: { params: { slug: string } }) => {
+  const { slug } = params;
+
+  const fileName = `./public/${slug}/index.md`;
+  const file = await readFile(fileName, 'utf8');
+
+  const { code, frontmatter } = await bundleMDX({
+    source: file,
+    mdxOptions(options, frontmatter) {
+      options.remarkPlugins = [...(options.remarkPlugins ?? [])];
+      options.rehypePlugins = [...(options.rehypePlugins ?? [])];
+
+      return options;
+    },
+  });
+
   return (
     <article>
-      <h1></h1>
+      <h1>{frontmatter.title}</h1>
+      <div>
+        <Post code={code} />
+      </div>
     </article>
   );
 };
 
 export default PostPage;
+
+const generateStaticParams = async () => {
+  const entries = await readdir('./public/', { withFileTypes: true });
+  const dirs = entries.filter(entry => entry.isDirectory()).map(entry => entry.name);
+
+  return dirs.map(dir => ({
+    slug: dir,
+  }));
+};
