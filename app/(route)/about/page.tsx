f9e588bc337fe681d2d6df4a10e59b9d24bcc39c
nknkcho <srirachacho@gmail.com>
2024-01-19 00:32:11 +0900

    Update: About 페이지 내용 추가

--- app/(route)/about/page.tsx
+++ app/(route)/about/page.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link';
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <h2 className="text-[28px] font-black">Namkyung Cho</h2>
+      <p className="mt-2">프론트엔드 개발자 조남경의 개발 블로그입니다.</p>
+      <div className="mt-2 text-[--link] hover:text-violet-600 hover:underline hover:underline-offset-4 w-fit">
+        <Link href="https://github.com/nknkcho" rel="noopener noreferrer" target="_blank">
+          GitHub
+        </Link>
+      </div>
+    </>
   );
 };
 
