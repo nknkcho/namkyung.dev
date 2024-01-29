2f6fbef4b8c7f17def2b78c5a0a55f98667a8c9b
nknkcho <srirachacho@gmail.com>
2024-01-29 23:28:48 +0900

    Update: apply dark mode

--- app/(route)/about/page.tsx
+++ app/(route)/about/page.tsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
+import Link from '../../Link';
 
 const About = () => {
   return (
     <>
       <h2 className="text-[28px] font-black">Namkyung Cho</h2>
       <p className="mt-2">프론트엔드 개발자 조남경의 개발 블로그입니다.</p>
-      <div className="mt-2 text-[--link] hover:text-violet-600 hover:underline hover:underline-offset-4 w-fit">
-        <Link href="https://github.com/nknkcho" rel="noopener noreferrer" target="_blank">
+      <div className="mt-2 text-[--link] hover:text-pink-400 hover:underline hover:underline-offset-4 w-fit dark:hover:text-violet-400">
+        <Link href="https://github.com/nknkcho" target="_blank">
           GitHub
         </Link>
       </div>
